325d1e80549f416349dbdc79510ed7a1a5b10751
tsole0 <locke.apogee@gmail.com>
2023-07-11 17:15:21 -0400

    WIP: attempted fixes of window resizing issues

--- ui_docViewer.py
+++ ui_docViewer.py
@@ -16,39 +16,61 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWebEngineWidgets import QWebEngineView
-from PySide6.QtWidgets import (QApplication, QDialog, QHBoxLayout, QPushButton,
-    QSizePolicy, QSpacerItem, QWidget)
+from PySide6.QtWidgets import (QApplication, QDialog, QGridLayout, QHBoxLayout,
+    QPushButton, QSizePolicy, QSpacerItem, QVBoxLayout,
+    QWidget)
 
 class Ui_docViewerWindow(object):
     def setupUi(self, docViewerWindow):
         if not docViewerWindow.objectName():
             docViewerWindow.setObjectName(u"docViewerWindow")
-        docViewerWindow.resize(711, 806)
-        self.horizontalLayoutWidget = QWidget(docViewerWindow)
-        self.horizontalLayoutWidget.setObjectName(u"horizontalLayoutWidget")
-        self.horizontalLayoutWidget.setGeometry(QRect(10, 770, 691, 31))
-        self.horizontalLayout = QHBoxLayout(self.horizontalLayoutWidget)
+        docViewerWindow.resize(710, 806)
+        sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(docViewerWindow.sizePolicy().hasHeightForWidth())
+        docViewerWindow.setSizePolicy(sizePolicy)
+        docViewerWindow.setMinimumSize(QSize(10, 10))
+        self.gridLayout = QGridLayout(docViewerWindow)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.horizontalLayout = QHBoxLayout()
         self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
         self.horizontalSpacer = QSpacerItem(35, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
 
         self.horizontalLayout.addItem(self.horizontalSpacer)
 
-        self.editButton = QPushButton(self.horizontalLayoutWidget)
+        self.editButton = QPushButton(docViewerWindow)
         self.editButton.setObjectName(u"editButton")
         self.editButton.setMinimumSize(QSize(93, 28))
 
         self.horizontalLayout.addWidget(self.editButton)
 
-        self.cancelButton = QPushButton(self.horizontalLayoutWidget)
+        self.cancelButton = QPushButton(docViewerWindow)
         self.cancelButton.setObjectName(u"cancelButton")
+        self.cancelButton.setEnabled(True)
         self.cancelButton.setMinimumSize(QSize(93, 28))
 
         self.horizontalLayout.addWidget(self.cancelButton)
 
+
+        self.gridLayout.addLayout(self.horizontalLayout, 2, 0, 1, 1)
+
+        self.verticalLayout = QVBoxLayout()
+        self.verticalLayout.setObjectName(u"verticalLayout")
         self.webEngineViewer = QWebEngineView(docViewerWindow)
         self.webEngineViewer.setObjectName(u"webEngineViewer")
-        self.webEngineViewer.setGeometry(QRect(9, 9, 691, 751))
+        sizePolicy1 = QSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.MinimumExpanding)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.webEngineViewer.sizePolicy().hasHeightForWidth())
+        self.webEngineViewer.setSizePolicy(sizePolicy1)
+        self.webEngineViewer.setMinimumSize(QSize(0, 740))
+
+        self.verticalLayout.addWidget(self.webEngineViewer)
+
+
+        self.gridLayout.addLayout(self.verticalLayout, 0, 0, 1, 1)
+
 
         self.retranslateUi(docViewerWindow)
 
