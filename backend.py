d45552d5f43688131e66cfb5b0a89f17a89da35f
tsole0 <locke.apogee@gmail.com>
2023-07-11 10:39:30 -0400

    backend.py now displays html

---
+++ backend.py
@@ -0,0 +1,29 @@
+import sys
+from os.path import abspath
+from PySide6 import QtGui, QtWebEngineWidgets, QtCore, QtWidgets
+
+
+# Load the HTML file
+app = QtWidgets.QApplication(sys.argv)
+
+
+class Window(QtWidgets.QMainWindow):
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.load_html()
+        # self.setupUi(self)
+        # self.connectSignalsSlots()
+
+    def load_html(self):
+        self.view = QtWebEngineWidgets.QWebEngineView()
+        html = abspath("my_docs.html")
+        self.view.setUrl(QtCore.QUrl.fromLocalFile(html))
+        # Show the web view in a window
+        self.setCentralWidget(self.view)
+        self.show()
+
+if __name__ == "__main__":
+    win = Window()
+    win.show()
+    app.exec()
