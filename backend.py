03d1a843c26fc1c3ea2545836918fcc3aebe54f2
tsole0 <locke.apogee@gmail.com>
2023-07-13 16:39:46 -0400

    incorporated qtwebenginecore

--- backend.py
+++ backend.py
@@ -1,6 +1,6 @@
 import sys
 from os.path import abspath
-from PySide6 import QtGui, QtWebEngineWidgets, QtCore, QtWidgets
+from PySide6 import QtGui, QtWebEngineWidgets, QtCore, QtWidgets, QtWebEngineCore
 from ui_docViewer import Ui_docViewerWindow
 
 
@@ -15,21 +15,24 @@ class Window(QtWidgets.QWidget, Ui_docViewerWindow):
     def load_html(self):
         html = abspath("my_docs.html")
         url = QtCore.QUrl.fromLocalFile(html)
-        profile = QtWebEngineWidgets.QWebEngineProfile.defaultProfile()
-        profile.setUrlRequestInterceptor(CustomUrlRequestInterceptor())
+        profile = CustomWebEngineProfile(self)
         self.webEngineViewer.setPage(QtWebEngineWidgets.QWebEnginePage(profile, self.webEngineViewer))
         self.webEngineViewer.load(url)
 
 
-class CustomUrlRequestInterceptor(QtWebEngineWidgets.QWebEngineUrlRequestInterceptor):
-    def interceptRequest(self, info):
-        url = info.requestUrl().toString()
+class CustomWebEngineProfile(QtWebEngineCore.QWebEngineProfile):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.requestStarted.connect(self.interceptRequest)
+
+    def interceptRequest(self, request):
+        url = request.url().toString()
         if url.startswith("http://") or url.startswith("https://"):
             # Allow loading external resources
-            info.setHttpHeader(b"Access-Control-Allow-Origin", b"*")
+            request.setHeader(b"Access-Control-Allow-Origin", b"*")
         else:
             # Allow loading local file resources
-            info.setHttpHeader(b"Access-Control-Allow-Origin", b"null")
+            request.setHeader(b"Access-Control-Allow-Origin", b"null")
 
 
 if __name__ == "__main__":
